5c3d24905bf9ae98ddee454794fd76a3d3a771c2
asapabhii <abhinavpwap@gmail.com>
2025-12-30 17:53:09 +0530

    feat(storage): define persistent storage interfaces

---
+++ src/storage/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Storage Module
+ *
+ * Storage interfaces and implementations for Cortex.
+ */
+
+export {
+  DEFAULT_STORAGE_CONFIG,
+  DistilledMemoryStorage,
+  FailureMemoryStorage,
+  IdentityStorage,
+  InMemoryDistilledStorage,
+  InMemoryFailureStorage,
+  InMemoryIdentityStorage,
+  StorageConfig,
+  StorageInitResult,
+} from './types.js';
+
